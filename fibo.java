f4fa400ec38d1721926fe1e6f6546c014aed6230
Juval Raj <47378887+juvalraj@users.noreply.github.com>
2020-02-28 16:01:07 +0530

    Create fibo.java

---
+++ fibo.java
@@ -0,0 +1,35 @@
+package s4csb;
+
+import java.util.Scanner;
+
+public class fibo {
+	int FirstNum;
+	int SecondNum;
+	int ThirdNum;
+	int Limit;
+	fibo(int n) {
+		Limit=n;
+		FirstNum=1;
+		SecondNum=0;
+	}
+	void generateFibonacii(){
+		for(int i=0;i<Limit;i++){
+			ThirdNum=FirstNum+SecondNum;
+			System.out.println(ThirdNum+" ");
+			FirstNum=SecondNum;
+			SecondNum=ThirdNum;
+			
+		}
+	}
+
+
+
+		public static void main(String[] args) {
+			Scanner sc =new Scanner(System.in);
+			System.out.println("Enter Limit");
+			int Limit=sc.nextInt();
+			fibo f = new fibo(Limit);
+			f.generateFibonacii();
+			}
+}
+	
